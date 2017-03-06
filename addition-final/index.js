b9c1dd5f95d4654d5caa9c0ee7e917d9faade90e
Jérémy Pinsolle <jpinsolle@xebia.fr>
2017-03-06 21:24:50 +0100

    addition-final code

--- addition-final/index.js
+++ addition-final/index.js
@@ -1,11 +1,21 @@
-'use strict';
 
 
-exports.handler = (event, context) => {
+module.exports.add = function (event, context) {
+  console.log(JSON.stringify(event));
 
-  const nb1 = event.nb1;
-  const nb2 = event.nb2;
-  const result = nb1 + nb2;
+  const n1 = event.n1;
+  const n2 = event.n2;
+  const r = n1 + n2;
 
-  context.succeed({result: result});
+  context.succeed({result: r});
+};
+
+module.exports.sub = function (event, context) {
+  console.log(JSON.stringify(event));
+
+  const n1 = event.n1;
+  const n2 = event.n2;
+  const r = n1 - n2;
+
+  context.succeed({result: r});
 };
